e7984dac01471d29cb7cfa7201d75c21d50662ce
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-05-14 01:12:37 -0700

    chap 1

---
+++ Front-End Development/JAVASCRIPT/Practice Questions/index.js
@@ -0,0 +1,56 @@
+// Chapter 1
+
+// Write a script to greet your website visitor using JS alert box.
+
+// alert('Hello World')
+
+// 2. Write a script to display following message on your web page:
+
+// alert('Error! Please enter a valid password')
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+// alert("Welcome to JS Land\nHappy Coding")
+
+// 4. Write a script to display following messages in sequence:
+
+// alert("Welcome to JS Land")
+// alert("Happy Coding")
+
+// 5. Generate the following message through browser’s developer console:
+
+// console.log("Hello, i run from your developer console")
+
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+
+//<script>
+// console.log("Hello, i run html file")
+// </script>
+
+// 7. Practice placement of <script></script> element in following sections of your project in exercise 6:
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+// <!DOCTYPE html>
+// <script>
+//     alert('Hey')
+// </script>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <script>
+//         alert('Hey')
+//     </script>
+// </head>
+// <body>
+//     <script>
+//         alert('Hey')
+//     </script>
+// </body>
+// <script>
+//     alert('Hey')
+// </script>
+// </html>
