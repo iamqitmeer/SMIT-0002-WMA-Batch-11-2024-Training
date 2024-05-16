108ce5af6a51d7a49132292a3e9c6177826fcd82
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-05-16 00:27:59 -0700

    chapters9-11

--- Front-End Development/JAVASCRIPT/Practice Questions/index.js
+++ Front-End Development/JAVASCRIPT/Practice Questions/index.js
@@ -493,3 +493,184 @@
 // </tr>
 // </table>
 // `);
+
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+
+// Chapter 6 TO 9
+
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+
+// Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this: “Welcome to city of lights”
+
+// let userInput = prompt("Enter City Name.");
+// userInput.toLowerCase() == "karachi" ? console.log("Welcome to city of lights") : console.log(`Welcome to ${userInput}`)
+
+// Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// let userInput = prompt("Enter Your Gender ");
+
+// if (userInput.toLowerCase() == "male") {
+//     console.log("Hey Sir")
+// } else if (userInput.toLowerCase() == "female") {
+//     console.log("Hello Ma'aam");
+// }else{
+//     console.log("Sorry");
+// }
+
+// Write a program to take input color of road traffic signal from the user & show the message according to this table:
+// Red - Must Stop
+// Yellow - Ready to move
+// Green - Move now
+
+// let userInput = prompt("Traffic Signal Color");
+
+// if (userInput.toLowerCase() == "red") {
+//   console.log("Must Stop");
+// } else if (userInput.toLowerCase() == "green") {
+//   console.log("Move now");
+// } else if (userInput.toLowerCase() == "yellow") {
+//   console.log("Ready to move");
+// } else {
+//   console.log("Not Found");
+// }
+
+// Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car”
+
+// let userInput = prompt("Enter remaining fuel in your car");
+// userInput == 0.25 ? console.log("Please refill the fuel in your car") : console.log("ChaleGa");
+
+// Run this script, & check whether alert message would be displayed or not. Record the outputs.
+// var a = 4;
+// if (++a === 5) {
+//   alert("a condition for variable a is true");
+// } // true
+// var b = 82;
+// if (b++ === 83) {
+//   alert("b condition for variable b is true");
+// } // false
+// var c = 12;
+// if (c++ === 13) {
+//   alert("condition c is true");
+// } // false
+// if (c === 13) {
+//   alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//   alert("condition 3 is true");
+// }
+// if (c === 14) {
+//   alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//   alert("The cost equals");
+// }
+// if (true) {
+//   alert("True");
+// }
+// if (false) {
+//   alert("False");
+// }
+// if ("car" < "cat") {
+//   alert("car is smaller than cat");
+// }
+
+// Write a program to take input the marks obtained in three subjects & total marks. Compute & show the resulting percentage on your page. Take percentage & compute grade as per following table: Show the total marks, marks obtained, percentage, grade & remarks like:
+
+// let totalMarks = 300;
+// let obtainedMarks = prompt("Ener Obtained marks");
+
+// let percentage = Math.ceil((obtainedMarks * 100) / totalMarks);
+// let grade = "";
+// let remarks = "";
+
+// if (percentage >= 80) {
+//   grade = "Excellent";
+//   remarks = "A1";
+// } else if (percentage >= 70) {
+//   grade = "Good";
+//   remarks = "A";
+// } else if (percentage >= 50) {
+//   grade = "Good";
+//   remarks = "You Need to Improve";
+// } else if (percentage <= 49) {
+//   grade = "Failed";
+//   remarks = "Sorry";
+// }
+
+// document.write(`
+// "Marks Sheet" <br>
+// Total Marks : <b>${totalMarks}</b> <br>
+// Obtained Marks : <b>${obtainedMarks}</b> <br>
+// Percentage : <b>${percentage}%</b> <br>
+// grade : <b>${grade}</b> <br>
+// remarks : <b>${remarks}</b>
+// `);
+
+// Guess game: Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number.
+// a. If user guesses the same number, show “Bingo! Correct answer”.
+// b. If the guessed number +1 is the secret number, show “Close enough to the correct answer”.
+
+// let guessNum = prompt("Guess Number")
+// let num = 5
+
+// if(guessNum == num){
+//     console.log("Bingo! Correct answer");
+// }else if(guessNum == ++num || guessNum == --num ){
+//     console.log("Close enough to the correct answer");
+// }else{
+//     console.log("Try Again");
+// }
+
+// Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.
+
+// let num = prompt("Enter Num")
+// num % 3 == 0 ? console.log("Divisible By 3") : console.log("Not Divisible Bu 3")
+
+// . Write a program that checks whether the given input is an even number or an odd number.
+
+// let num = prompt("Enter Num")
+// if (num % 2 == 0) {
+//     console.log(`${num} is Even`);
+// }else {
+//     console.log(`${num} is Odd`);
+// }
+
+// Write a program that takes temperature as input and shows a message based on following criteria
+// a. T > 40 then “It is too hot outside.”
+// b. T > 30 then “The Weather today is Normal.”
+// c. T > 20 then “Today’s Weather is cool.”
+// d. T > 10 then “OMG! Today’s weather is so Cool.”
+
+// let temperature = prompt("Enter Temprature")
+
+// if (temperature >= 40) {
+//     console.log("It is too hot outside.");
+// } else if (temperature >= 30) {
+//     console.log("The Weather today is Normal.");
+// }else if (temperature >= 20) {
+//     console.log("Today’s Weather is cool.");
+// }else if (temperature >= 10 || temperature < 10) {
+//     console.log("OMG! Today’s weather is so Cool.");
+// }
+
+// Write a program to create a calculator for +,-,*, / & % using if statements. Take the following input:
+// a. First number
+// b. Second number
+// c. Operation (+, -, *, /, %)
+// Compute & show the calculated result to user.
+
+// let first = +prompt("First number");
+// let sec = +prompt("Second number");
+// let operation = prompt("Enter Operation");
+
+// if (operation == "+") console.log(first + sec);
+// if (operation == "-") console.log(first - sec);
+// if (operation == "/") console.log(first / sec);
+// if (operation == "*") console.log(first * sec);
