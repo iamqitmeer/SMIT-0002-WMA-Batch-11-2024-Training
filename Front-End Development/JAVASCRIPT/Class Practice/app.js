386e4b780a4696122b41f75a649cfad2d5b44515
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-06-05 19:07:28 -0700

    basic changes

---
+++ Front-End Development/JAVASCRIPT/Class Practice/app.js
@@ -0,0 +1,20 @@
+// let arr = [11, 13, 14, 15, 20];
+
+// for (let i = 0; i < arr.length; i++) {
+//   let diff = arr[i + 1] - arr[i];
+//   if (diff > 1) {
+//     for (let j = 1; j < diff; j++) {
+//       console.log(arr[i] + j);
+//     }
+//   }
+// }
+
+// console.log(arr);
+
+// let userInput = prompt("Enter Word");
+// let word = ""
+// for (let i = userInput.length - 1; i >= 0; i--) {
+//   word += userInput[i];
+// }
+
+// console.log(word);
