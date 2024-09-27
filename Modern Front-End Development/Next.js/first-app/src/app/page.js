b95f07b20417c74a34d64f1207577e5d3d6d80e7
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-09-27 11:32:28 +0500

    push code

---
+++ Modern Front-End Development/Next.js/first-app/src/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-72 h-72 bg-violet-600 rounded-3xl shadow- shadow-violet-950"></div>
+    </div>
+  );
+}
+
+export default Home;
