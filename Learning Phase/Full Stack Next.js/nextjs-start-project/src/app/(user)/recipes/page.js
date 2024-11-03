c5542eab13fd3a3227f1694548dd857b5e5e1c91
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-11-03 13:00:35 +0500

    fetch data

--- Learning Phase/Full Stack Next.js/nextjs-start-project/src/app/(user)/recipes/page.js
+++ Learning Phase/Full Stack Next.js/nextjs-start-project/src/app/(user)/recipes/page.js
@@ -1,74 +1,21 @@
-import Link from "next/link";
+import Card from "@/components/Card";
+import Image from "next/image";
 import React from "react";
 
-function recipes() {
-  const cricketers = [
-    {
-      name: "Babar Azam",
-      slug: "babar-azam",
-      country: "Pakistan",
-      role: "Batsman",
-      battingStyle: "Right-hand bat",
-      bowlingStyle: "Right-arm off-break",
-    },
-    {
-      name: "Imran Khan",
-      slug: "imran-khan",
-      country: "Pakistan",
-      role: "All-rounder",
-      battingStyle: "Right-hand bat",
-      bowlingStyle: "Right-arm fast",
-    },
-    {
-      name: "Wasim Akram",
-      slug: "wasim-akram",
-      country: "Pakistan",
-      role: "Bowler",
-      battingStyle: "Left-hand bat",
-      bowlingStyle: "Left-arm fast",
-    },
+export const metadata = {
+  title: "Recipes",
+};
 
-    {
-      name: "Shaheen Afridi",
-      slug: "shaheen-afridi",
-      country: "Pakistan",
-      role: "Bowler",
-      battingStyle: "Left-hand bat",
-      bowlingStyle: "Left-arm fast",
-    },
-    {
-      name: "Shoaib Akhtar",
-      slug: "shoaib-akhtar",
-      country: "Pakistan",
-      role: "Bowler",
-      battingStyle: "Right-hand bat",
-      bowlingStyle: "Right-arm fast",
-    },
-    {
-      name: "Shahid Afridi",
-      slug: "shahid-afridi",
-      country: "Pakistan",
-      role: "All-rounder",
-      battingStyle: "Right-hand bat",
-      bowlingStyle: "Right-arm leg-spin",
-    },
-  ];
+const Recipes = async () => {
+  let response = await fetch("https://dummyjson.com/recipes");
+  let data = await response.json();
   return (
-    <div className="flex items-center justify-center flex-col p-20 gap-4">
-      {cricketers.map((cricketer) => {
-        return (
-          <div key={cricketer.slug}>
-            <Link
-              className="text-zinc-900 border-2 border-zinc-900 text-xl font-bold hover:bg-zinc-900 hover:text-white p-2 px-3 rounded-lg m-4"
-              href={`/recipes/${cricketer.slug}`}
-            >
-              {cricketer.name}
-            </Link>
-          </div>
-        );
+    <div className="flex items-center justify-center flex-wrap gap-4 p-12 w-full h-screen">
+      {data.recipes && data.recipes.map((recipe) => {
+        return <Card key={recipe.id} recipe={recipe} />
       })}
     </div>
   );
-}
+};
 
-export default recipes;
+export default Recipes;
