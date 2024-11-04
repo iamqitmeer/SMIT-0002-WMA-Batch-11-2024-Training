f60a632a7daac7a0e098b630375d80da6aa2c4aa
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-11-04 16:14:01 +0500

    basic changes

--- Learning Phase/Full Stack Next.js/nextjs-start-project/src/components/Card.jsx
+++ Learning Phase/Full Stack Next.js/nextjs-start-project/src/components/Card.jsx
@@ -3,19 +3,15 @@ import Image from "next/image";
 const Card = ({ recipe }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-      {/* Image Section */}
       <div className="relative">
         <Image
-          src={recipe.image} // Use recipe image from data
+          src={recipe.image} 
           alt={recipe.name}
-          width={192} // Tailored width
-          height={192} // Tailored height
-          className="rounded-lg opacity-90" // Rounded corners and reduced opacity
-          style={{ objectFit: "cover" }} // Ensure image covers the area
-        />
+          width={192} 
+          height={192}
+          className="rounded-lg opacity-90" style={{ objectFit: "cover" }}/>
       </div>
 
-      {/* Content Section */}
       <div className="p-4">
         <h2 className="text-lg font-semibold text-gray-800">{recipe.name}</h2>
       </div>
