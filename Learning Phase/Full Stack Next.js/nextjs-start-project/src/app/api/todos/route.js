c00f993c7d53bd0be390b5228158acee5a19fcc3
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-11-06 22:23:04 +0500

    basic changes

--- Learning Phase/Full Stack Next.js/nextjs-start-project/src/app/api/todos/route.js
+++ Learning Phase/Full Stack Next.js/nextjs-start-project/src/app/api/todos/route.js
@@ -1,10 +1,4 @@
-const todos = [
-  { id: 1, title: "Learn JavaScript", completed: false },
-  { id: 2, title: "Practice React", completed: true },
-  { id: 3, title: "Build a To-Do App", completed: false },
-  { id: 4, title: "Read about Node.js", completed: false },
-  { id: 5, title: "Explore MongoDB", completed: true },
-];
+const todos = [];
 
 export async function GET() {
   return Response.json({ statusCode: "404", todos });
